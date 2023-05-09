5eebe5bd3b8ef45a5d5c594df19e75a5e377d4f2
Nikola Stefanovski <nikoding32@gmail.com>
2023-05-09 21:07:51 +0200

    added reset turn and disable card

--- src/App.js
+++ src/App.js
@@ -16,6 +16,7 @@ function App() {
   const [turns, setTurns] = useState (0);
   const [choiceOne, setChoiceOne] = useState(null);
   const [choiceTwo, setChoiceTwo] = useState(null);
+  const [disabled, setDisabled] = useState(false);
 
   //shuffle cards
   const shuffleCards = () => {
@@ -23,6 +24,8 @@ function App() {
       .sort(() => Math.random() - 0.5)
       .map((card) => ({ ...card, id: Math.random() }))
 
+      setChoiceOne(null)
+      setChoiceTwo(null)
       setCards(shuffledCards)
       setTurns(0)
   }
@@ -35,6 +38,7 @@ function App() {
   // handle a match
   useEffect (() => {
     if (choiceOne && choiceTwo) {
+      setDisabled(true)
     if (choiceOne.src === choiceTwo.src) {
       setCards(prevCards => {
         return prevCards.map(card => {
@@ -58,7 +62,13 @@ function App() {
     setChoiceOne(null)
     setChoiceTwo(null)
     setTurns(prevTurns => prevTurns + 1)
+    setDisabled(false)
   }
+
+  // Start a new game automatically 
+  useEffect (() => {
+    shuffleCards()
+  }, [])
 
   return (
     <div className="App">
@@ -72,9 +82,11 @@ function App() {
             card={card}
             handleChoice={handleChoice}
             flipped={card === choiceOne || card === choiceTwo || card.matched}
+            disabled={disabled}
           />
         ))}
       </div>
+      <p>Turns: {turns}</p>
     </div>
   );
 }
