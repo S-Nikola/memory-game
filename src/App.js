33a3af810c996d6bc7fd7053c4b6f262c0fe289b
Nikola Stefanovski <nikoding32@gmail.com>
2023-05-12 13:28:34 +0200

    added sound and new levels

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import { useState, useEffect } from 'react';
 import './App.css';
 import SingleCard from './components/SingleCard';
+import success from './sound/success.wav';
+import match from './sound/match.wav';
 
 const cardImagesLvlOne = [
   {"src": "memory-game/img/helmet-1.png", matched: false},
@@ -22,12 +24,48 @@ const cardImagesLvlTwo = [
   {"src": "memory-game/img/bag-1.png", matched: false},
 ]
 
+const cardImagesLvlThree = [
+  {"src": "memory-game/img/helmet-1.png", matched: false},
+  {"src": "memory-game/img/potion-1.png", matched: false},
+  {"src": "memory-game/img/ring-1.png", matched: false},
+  {"src": "memory-game/img/scroll-1.png", matched: false},
+  {"src": "memory-game/img/shield-1.png", matched: false},
+  {"src": "memory-game/img/sword-1.png", matched: false},
+  {"src": "memory-game/img/axe-1.png", matched: false},
+  {"src": "memory-game/img/bag-1.png", matched: false},
+  {"src": "memory-game/img/book-1.png", matched: false},
+  {"src": "memory-game/img/bow-1.png", matched: false},
+  {"src": "memory-game/img/candle-1.png", matched: false},
+  {"src": "memory-game/img/coins-1.png", matched: false},
+]
+
+const cardImagesLvlFour = [
+  {"src": "memory-game/img/helmet-1.png", matched: false},
+  {"src": "memory-game/img/potion-1.png", matched: false},
+  {"src": "memory-game/img/ring-1.png", matched: false},
+  {"src": "memory-game/img/scroll-1.png", matched: false},
+  {"src": "memory-game/img/shield-1.png", matched: false},
+  {"src": "memory-game/img/sword-1.png", matched: false},
+  {"src": "memory-game/img/axe-1.png", matched: false},
+  {"src": "memory-game/img/bag-1.png", matched: false},
+  {"src": "memory-game/img/book-1.png", matched: false},
+  {"src": "memory-game/img/bow-1.png", matched: false},
+  {"src": "memory-game/img/candle-1.png", matched: false},
+  {"src": "memory-game/img/coins-1.png", matched: false},
+  {"src": "memory-game/img/hat-1.png", matched: false},
+  {"src": "memory-game/img/quill-1.png", matched: false},
+]
+
+
+
 function App() {
   const [cards, setCards] = useState([]);
   const [turns, setTurns] = useState (0);
   const [choiceOne, setChoiceOne] = useState(null);
   const [choiceTwo, setChoiceTwo] = useState(null);
   const [disabled, setDisabled] = useState(false);
+  const [matchedCount, setMatchedCount] = useState(0);
+  const [soundEnabled, setSoundEnabled] = useState(false);
 
   //shuffle cards
   const shuffleCardsLvlOne = () => {
@@ -52,32 +90,75 @@ function App() {
       setTurns(0)
   }
 
+  const shuffleCardsLvlThree = () => {
+    const shuffledCards = [...cardImagesLvlThree, ...cardImagesLvlThree]
+      .sort(() => Math.random() - 0.5)
+      .map((card) => ({ ...card, id: Math.random() }))
+
+      setChoiceOne(null)
+      setChoiceTwo(null)
+      setCards(shuffledCards)
+      setTurns(0)
+  }
+
+  const shuffleCardsLvlFour = () => {
+    const shuffledCards = [...cardImagesLvlFour, ...cardImagesLvlFour]
+      .sort(() => Math.random() - 0.5)
+      .map((card) => ({ ...card, id: Math.random() }))
+
+      setChoiceOne(null)
+      setChoiceTwo(null)
+      setCards(shuffledCards)
+      setTurns(0)
+  }
+
   // handle a choice
   const handleChoice = (card) => {
     choiceOne ? setChoiceTwo(card) : setChoiceOne(card)
   }
 
-  // handle a match
-  useEffect (() => {
-    if (choiceOne && choiceTwo) {
-      setDisabled(true)
+// sound effects
+useEffect(() => {
+  if (matchedCount === cards.length) {
+    if (soundEnabled) {
+      const successSound = new Audio(success);
+      successSound.play();
+    } 
+  }
+}, [matchedCount, cards.length, soundEnabled]);
+
+// handle a match
+useEffect (() => {
+
+  //sound effect fir matching
+  function matchSound () {
+    if (soundEnabled) {
+      const matchSound = new Audio(match);
+      matchSound.play();
+    }
+  }
+  //handling the match
+  if (choiceOne && choiceTwo) {
+    setDisabled(true)
     if (choiceOne.src === choiceTwo.src) {
       setCards(prevCards => {
         return prevCards.map(card => {
           if (card.src === choiceOne.src) {
+            matchSound()
             return {...card, matched: true}
           } else {
             return card
           }
         })
       })
+      setMatchedCount(prevCount => prevCount + 2);
       resetTurn()
     }
     else {
       setTimeout(() => resetTurn(), 1000)
-      }
     }
-  }, [choiceOne, choiceTwo])
+  }
+}, [choiceOne, choiceTwo, soundEnabled])
 
   // reset choices & increase turn
   const resetTurn = () => {
@@ -95,8 +176,16 @@ function App() {
   return (
     <div className="App">
       <h1>Memory game</h1>
+      <button onClick={() => setSoundEnabled(!soundEnabled)}>
+        {soundEnabled ? 'Disable Sound' : 'Enable Sound'}
+      </button>
+      <br />
       <button onClick={shuffleCardsLvlOne}>Level One</button>
       <button onClick={shuffleCardsLvlTwo}>Level Two</button>
+      <button onClick={shuffleCardsLvlThree}>Level Three</button>
+      <button onClick={shuffleCardsLvlFour}>Level Four</button>
+      <br />
+      <p>Turns: {turns}</p>
 
       <div className='card-grid'>
         {cards.map(card => (
@@ -109,7 +198,6 @@ function App() {
           />
         ))}
       </div>
-      <p>Turns: {turns}</p>
     </div>
   );
 }
